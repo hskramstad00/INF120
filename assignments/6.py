c46528be2044b46727af3ad95c4ac7b3458e0fda
hskramstad00 <hakon.skramstad@nmbu.no>
2026-01-04 09:57:54 +0100

    six finished

--- assignments/6.py
+++ assignments/6.py
@@ -7,6 +7,9 @@ text = 'data/norec_corpus.txt'
 decoding_text = open(text, 'r', encoding='utf-8')
 
 letters = {}
+# can also use sum(letters.value()), but since assignment specifies using a variabel to
+# count number of letters
+total_letters = 0
 exclude_chars = [
     ' ', '\n', ',', '.', '-', '–', '—', '*', '(', ')',
     '«', '»', ':', ';', '’', '?', "'", '"', '/', '!', '…',
@@ -15,15 +18,28 @@ exclude_chars = [
 for line in decoding_text:
     line = line.strip()
 
-    # convert all to lower
-    line = line.lower()
+    # convert all to upper
+    line = line.upper()
 
     for chars in line:
         if chars not in exclude_chars:
+            total_letters += 1
             if chars in letters:
                 letters[chars] = letters[chars] + 1
             else:
                 letters[chars] =1
 
-for key in list(letters.keys()):
-    print(key, " ", letters[key])
+# making the dicitonary with realtive frequency
+relative_frequency = {keys: values / total_letters *100 for keys, values in letters.items()}
+
+# changing from dict to tuples
+tuples_list = list(relative_frequency.items())
+
+def order_by(tup):
+    return tup[1]
+
+sorted_tuples = sorted(tuples_list, key=order_by, reverse=True)
+
+print(f'Counted letters: {total_letters}')
+for x,y in sorted_tuples:
+    print(f'{x} {y:.2f}%')
